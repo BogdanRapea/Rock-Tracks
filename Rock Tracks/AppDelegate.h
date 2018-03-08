0cbb8328b80941b867e99cd3e8b03ff28999d861
Bogdan <bogdan@Bogdans-iMac.local>
2018-03-08 11:54:41 -0800

    Initial Commit

---
+++ Rock Tracks/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Rock Tracks
+//
+//  Created by Bogdan on 08/03/2018.
+//  Copyright © 2018 Bogdan Rapea. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Rock Tracks/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Rock Tracks
+//
+//  Created by Bogdan on 08/03/2018.
+//  Copyright © 2018 Bogdan Rapea. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
